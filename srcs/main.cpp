ff7fbc98da9059e6baf732214e1f3f1fc5ccdc00
Lyfmeno <hugolevipro@gmail.com>
2023-02-07 16:57:40 +0100

    start of parsing - getting lines and cutting newlines or empty

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,11 +6,27 @@
 /*   By: hlevi <hlevi@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 16:11:25 by hlevi             #+#    #+#             */
-/*   Updated: 2023/02/07 15:00:38 by hlevi            ###   ########.fr       */
+/*   Updated: 2023/02/07 16:48:14 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/Webserv.hpp"
+#include <algorithm>
+
+int	parsing(ft::Webserv base)
+{
+    if (base.open_file())
+        return (-1);
+    while (base._file)
+    {
+        std::getline(base._file, base._line, '\n');
+		base._line.erase(std::remove(base._line.begin(), base._line.end(), '\n'), base._line.end());
+		if (!base._line.empty())
+			std::cout << base._line << std::endl;
+    }
+    base._file.close();
+	return (0);
+}
 
 int main(int ac, char **av)
 {
@@ -19,13 +35,7 @@ int main(int ac, char **av)
 	if (ac != 2)
 		return (-1);
 	base._filename = av[1];
-    if (base.open_file())
-        return (-1);
-    while (base._file)
-    {
-        std::getline(base._file, base._line, ';');
-        std::cout << "| " << base._line << " |" << std::endl;
-    }
-    base._file.close();
+	if (parsing(base))
+		return (-1);
     return (0);
 }
