3b87250061cce9b651a2b7309b9f0fa35a4e04fd
Lyfmeno <hugolevipro@gmail.com>
2023-03-13 12:06:39 +0100

    Better main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,31 +6,23 @@
 /*   By: avarnier <avarnier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 16:11:25 by hlevi             #+#    #+#             */
-/*   Updated: 2023/03/08 13:37:39 by avarnier         ###   ########.fr       */
+/*   Updated: 2023/03/13 12:06:12 by hlevi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/Webserv.hpp"
 #include <exception>
 
-
 int main(int ac, char **av)
 {
-	if (ac != 2)
-	{
-		std::cout << "Webserv only need one .conf" << '\n';
+	if (ac != 2) {
+		std::cerr << "\033[31mError: webserv takes a configuration file as an argument\033[0m\n";
 		return (-1);
 	}
-
-	try
-	{
+	try {
 		ft::Webserv	engine(av[1]);
 		engine.run();
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-
+	} 
+	catch (std::exception &e) { std::cout << "\033[31m" << e.what() << "\033[0m" << "\n"; }
     return (0);
 }
